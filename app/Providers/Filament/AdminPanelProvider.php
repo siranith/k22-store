4b3c885e5ed9c8d14e7a2e42724caee91c7d4dc0
Nith <Nith@DESKTOP-18DPTB7>
2025-10-31 22:57:50 +0700

    add widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Widgets\SalesSummaryWidget;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -37,9 +38,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
+            SalesSummaryWidget::class, // 👈 Add this line
+        ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -55,4 +55,10 @@ class AdminPanelProvider extends PanelProvider
                 Authenticate::class,
             ]);
     }
+    protected function getWidgets(): array
+{
+    return [
+        SalesSummaryWidget::class,
+    ];
+}
 }
